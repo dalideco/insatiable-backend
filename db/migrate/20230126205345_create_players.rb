594689ee71e819ae5235bd5d25501575c7656d6a
dalideco <dalissj1@gmail.com>
2023-01-27 00:24:42 +0100

    setting index on emai

--- db/migrate/20230126205345_create_players.rb
+++ db/migrate/20230126205345_create_players.rb
@@ -1,12 +1,10 @@
 class CreatePlayers < ActiveRecord::Migration[7.0]
   def change
     create_table :players do |t|
-      t.string :email
+      t.string :email, index: { unique: true }
       t.string :password
       t.string :avatar, not_null: false
       t.timestamps
     end
-
-    add_index :players, :email
   end
 end
