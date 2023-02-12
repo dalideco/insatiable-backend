8ce003613f53df362b4647d071cc9d91e1e4fd6f
dalideco <dalissj1@gmail.com>
2023-02-12 14:42:41 +0100

    schema for default timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_181003) do
     t.integer "minimum_bid", null: false
     t.integer "current_bid"
     t.integer "buy_now_price", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", default: "2023-02-12 13:42:07", null: false
+    t.datetime "updated_at", default: "2023-02-12 13:42:07", null: false
     t.integer "player_id"
     t.integer "weapon_id"
   end
@@ -24,21 +24,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_181003) do
   create_table "own_packs", force: :cascade do |t|
     t.integer "player_id"
     t.integer "pack_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", default: "2023-02-12 13:42:07", null: false
+    t.datetime "updated_at", default: "2023-02-12 13:42:07", null: false
   end
 
   create_table "owns", force: :cascade do |t|
     t.integer "player_id"
     t.integer "weapon_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", default: "2023-02-12 13:42:07", null: false
+    t.datetime "updated_at", default: "2023-02-12 13:42:07", null: false
   end
 
   create_table "packs", force: :cascade do |t|
     t.integer "price", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", default: "2023-02-12 13:42:07", null: false
+    t.datetime "updated_at", default: "2023-02-12 13:42:07", null: false
   end
 
   create_table "players", force: :cascade do |t|
@@ -46,16 +46,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_181003) do
     t.string "password_digest", null: false
     t.string "avatar"
     t.datetime "confirmed_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", default: "2023-02-12 13:42:07", null: false
+    t.datetime "updated_at", default: "2023-02-12 13:42:07", null: false
     t.index ["email"], name: "index_players_on_email", unique: true
   end
 
   create_table "weapons", force: :cascade do |t|
     t.string "image"
     t.string "stats"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", default: "2023-02-12 13:42:07", null: false
+    t.datetime "updated_at", default: "2023-02-12 13:42:07", null: false
     t.integer "variant", default: 0
   end
 
