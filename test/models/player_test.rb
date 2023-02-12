6be179c3cbf392fd5a29332b366993c9571daa10
dalideco <dalissj1@gmail.com>
2023-02-12 15:02:03 +0100

    adding player tests

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,7 +1,36 @@
-require "test_helper"
+require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include V1
+
+  # Create tests
+  test 'Should not create player without email and password' do
+    player = Player.new
+    assert_not player.save
+  end
+
+  test 'Shoud not created player without password' do
+    player = Player.new(email: 'test@test.test')
+    assert_not player.save
+  end
+
+  test 'Should not createdplayer without password confirmation' do
+    # TODO
+  end
+
+  test 'Should not create player with wrong password confirmation' do
+    # TODO
+  end
+
+  test 'Should create a player with email and password with p confirmation' do
+    player = Player.new(email: 'test@test.test', password: 'test', password_confirmation: 'test')
+    assert player.save
+  end
+
+  test 'Should not create two players with same email' do
+    player = Player.new(email: 'test@test.test', password: 'test', password_confirmation: 'test')
+    assert player.save
+    player1 = Player.new(email: 'test@test.test', password: 'testing', password_confirmation: 'testing')
+    assert_not player1.save
+  end
 end
