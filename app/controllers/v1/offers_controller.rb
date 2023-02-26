1be2a07b7da26e4ffd343f9639cb175350f1707e
dalideco <dalissj1@gmail.com>
2023-02-26 17:11:54 +0100

    player must own weapon in order to create an offer

--- app/controllers/v1/offers_controller.rb
+++ app/controllers/v1/offers_controller.rb
@@ -3,6 +3,7 @@ module V1
   class OffersController < ApplicationController
     before_action :authorize_request
     before_action :find_offer, except: %i[create index]
+    before_action :check_player_owns_weapon, only: :create
 
     def index
       render json: Offer.all
@@ -35,6 +36,9 @@ module V1
     private
 
     def offer_create_params
+      params.require(:minimum_bid)
+      params.require(:buy_now_price)
+      params.require(:weapon_id)
       params.permit(:minimum_bid, :buy_now_price, :weapon_id)
     end
 
@@ -47,6 +51,16 @@ module V1
     rescue ActiveRecord::RecordNotFound
       render status: :not_found, json: {
         error: 'Offer not found'
+      }
+    end
+
+    def check_player_owns_weapon
+      weapon_id = offer_create_params[:weapon_id]
+
+      Own.find_by!(weapon_id:, player_id: @current_player.id)
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, json: {
+        error: "Player #{@current_player.id} does not own weapon #{weapon_id}"
       }
     end
   end
