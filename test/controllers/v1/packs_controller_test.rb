ec070008846715606381070803e67d3b88903e05
dalideco <dalissj1@gmail.com>
2023-03-24 02:32:32 +0100

    defining tests for packs controller

--- test/controllers/v1/packs_controller_test.rb
+++ test/controllers/v1/packs_controller_test.rb
@@ -2,5 +2,13 @@ require 'test_helper'
 
 module V1
   class PacksControllerTest < ActionDispatch::IntegrationTest
+    test 'Buy: Should buy player' do
+    end
+    test 'Buy: Should not buy an inexistant pack' do
+    end
+    test 'Buy: Should not buy when unauthenticated' do
+    end
+    test 'Buy: Should not buy when player does not have enough coins' do
+    end
   end
 end
