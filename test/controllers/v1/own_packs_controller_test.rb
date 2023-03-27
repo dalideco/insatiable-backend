e7b7a0c30b6d8425cabe2823dd0c0119c8185cc0
dalideco <dalissj1@gmail.com>
2023-03-27 23:40:06 +0100

    updates to open tests

--- test/controllers/v1/own_packs_controller_test.rb
+++ test/controllers/v1/own_packs_controller_test.rb
@@ -5,20 +5,29 @@ module V1
     setup do
       @own_pack = own_packs(:one)
       @own_pack_not_owned = own_packs(:two)
+
+      @pack = packs(:one)
     end
 
     test 'Open: Should open a pack' do
       authenticate
 
       post :open, params: {
-        id: @own_pack.id
+        pack_id: @own_pack.id
       }
 
       assert_response :success
+      response_body = JSON.parse(response.body)
 
+      response_weapon = response_body['weapon']
       # verify owned pack has been removed once opened
       own_pack = OwnPack.find_by(id: @own_pack.id)
       assert_nil own_pack
+
+      # verify the the player now owns the weapon
+      owned_weapons = @player.owned_weapons
+      weapon = owned_weapons.select { |wep| wep.id == response_weapon.id }
+      assert_not_nil weapon
     end
     test 'Open: Should not open a pack when is not authenticated' do
       post :open, params: {
