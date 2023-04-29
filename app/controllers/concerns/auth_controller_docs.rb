4347893d0ea2af7f8d58a63e9b5488b9178eef34
dalideco <dalissj1@gmail.com>
2023-04-29 18:21:50 +0100

    whoami docs

--- app/controllers/concerns/auth_controller_docs.rb
+++ app/controllers/concerns/auth_controller_docs.rb
@@ -11,4 +11,13 @@ module AuthControllerDocs
     property :email, String, desc: 'Email of the authenticated player'
   end
   def login; end
+
+  api :GET, '/auth/whoami'
+  description 'identifying user'
+  error :unauthorized, 'Invalid web token'
+  returns code: :ok do
+    property :success, [true, false]
+    property :player, Hash, desc: 'authenticated player, contains also the owned weapons and packs'
+  end
+  def whoami; end
 end
