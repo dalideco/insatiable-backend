01125da82e4f6fb3d2075247e9d6a0b97c9304e2
dalideco <dalissj1@gmail.com>
2023-04-30 19:02:48 +0100

    offer moves money and ownership of weapon

--- app/jobs/offer_cleanup_job.rb
+++ app/jobs/offer_cleanup_job.rb
@@ -8,12 +8,35 @@ class OfferCleanupJob < ApplicationJob
     # Do something later
     Rails.logger.info("cleaning offer #{offer.id}")
 
-    return if offer.sold? || offer.expired?
+    # stop if offer is expired or sold
+    if offer.sold? || offer.expired?
+      Rails.logger.info("offer #{offer.id} already sold or expired")
+      return
+    end
 
-    offer.update(status: :expired) unless offer.bid?
+    # offer just expires if no bidding
+    unless offer.bid?
+      Rails.logger.info("offer #{offer.id} not bidded on, it goes expired")
+      offer.update(status: :expired)
+      return
+    end
 
-    return unless offer.bid?
+    # if bidding, transaction is made and offer is sold
+    ActiveRecord::Base.transaction do
+      offer.player.update(coins: offer.player.coins + offer.current_bid)
+      Rails.logger.info("offer #{offer.id}: coins are moved to #{offer.player.id} ")
 
-    offer.update(status: :sold)
+      offer.update(status: :sold)
+      Rails.logger.info("offer #{offer.id} is sold to the latest bidder")
+
+      own = V1::Own.new(
+        player_id: offer.latest_bidder.id,
+        weapon_id: offer.weapon.id
+      )
+      own.save!
+      Rails.logger.info("offer #{offer.id}: weapon ownership is passed to player #{offer.latest_bidder.id}")
+    rescue ActiveRecord::RecordInvalid
+      Rails.logger.error("offer #{offer.id}: transaction failed")
+    end
   end
 end
