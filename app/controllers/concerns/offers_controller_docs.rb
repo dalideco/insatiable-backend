8f7e25d2af1b1e722f07a464e3da22cca7425c1a
dalideco <dalissj1@gmail.com>
2023-04-30 19:13:41 +0100

    adding lifetime to offers docs

--- app/controllers/concerns/offers_controller_docs.rb
+++ app/controllers/concerns/offers_controller_docs.rb
@@ -9,10 +9,19 @@ module OffersControllerDocs
   param :minimum_bid, :number, required: true
   param :buy_now_price, :number, required: true
   param :weapon_id, :number, required: true
+  param :lifetime, [
+    '1.hour',
+    '3.hours',
+    '6.hours',
+    '12.hours',
+    '1.day',
+    '3.days',
+    '30.seconds'
+  ], required: true
   returns code: :ok do
     property :success, [true, false], desc: 'Whether the creationg was successful'
     property :offer, Hash, desc: 'Created offer object' do
-      property :id, String, desc: 'The player\'s id'
+      property :success, [true, false], desc: 'The player\'s id'
       property :minimum_bid, String, desc: 'Minimum bid for the offer'
       property :buy_now_price, String, desc: 'The buy now price'
       property :current_bid, String, desc: 'latest bid', required: false
